c0f7302a9e1e7ab1f88ccf444c9cf9bfa2e9e540
Jura19777 <poryadin77@gmail.com>
2023-01-22 16:59:07 +0700

    квадратом другого числа - ответ

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.Clear();
+Console.Write("Введите первое число : ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b) Console.WriteLine("Второе число : " + b + " является квадратом числа " + a);
+else
+    if (b * b == a) Console.WriteLine("Первое число : " + a + " является квадратом числа " + b);
+else Console.WriteLine("Ни одно число не является квадратом другого числа - " + b +", "+ a);
